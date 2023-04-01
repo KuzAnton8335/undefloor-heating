4b2bb22bc4a1579e7a022dc9832388f98c2cc932
KuzAnton8335 <ant2783@mail.com>
2023-04-01 17:02:04 +0300

    add style add js add images add fonts

---
+++ js/main.js
@@ -0,0 +1,124 @@
+const swiper = new Swiper('.hero__slider', {
+	slidesPerView: 2,
+	spaceBetween: 10,
+	loop: true,
+	navigation: {
+		prevEl: '.hero__slider-btn-prev',
+		nextEl: '.hero__slider-btn-next'
+	},
+	autoplay: {
+		delay: 3000,
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1
+
+		},
+		560: {
+			spaceBetween: 8,
+		}
+	}
+});
+
+// калькулятор
+
+const calcForm = document.querySelector('.js-calc-form');
+const totalSquare = document.querySelector('.js-squar');
+const totalPrice = document.querySelector('.js-total-price');
+const calcWrapper = document.querySelector('.calc__result--wrapper');
+
+const tariff = {
+	economy: 550,
+	comfort: 1400,
+	premium: 2700,
+};
+
+calcForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	console.log(calcForm.width.value)
+	console.log(calcForm.lenght.value)
+	console.log(calcForm.tariff.value)
+
+	if (calcForm.width.value > 0 && calcForm.lenght.value) {
+		const square = calcForm.width.value * calcForm.lenght.value;
+		const price = square * tariff[calcForm.tariff.value];
+
+		calcWrapper.style.display = 'block';
+
+		totalSquare.textContent = `${square} кв м`
+		totalPrice.textContent = `${price} руб`
+	}
+});
+
+// modal popup
+const scrollController = {
+	scrollPosition: 0,
+	disabledScroll() {
+		scrollController.scrollPosition = window.scrollY;
+		document.body.style.cssText = `
+      overflow: hidden;
+      position: fixed;
+      top: -${scrollController.scrollPosition}px;
+      left: 0;
+      height: 100vh;
+      width: 100vw;
+      padding-right: ${window.innerWidth - document.body.offsetWidth}px
+    `;
+		document.documentElement.style.scrollBehavior = 'unset';
+	},
+	enabledScroll() {
+		document.body.style.cssText = '';
+		window.scroll({ top: scrollController.scrollPosition })
+		document.documentElement.style.scrollBehavior = '';
+	},
+}
+
+
+const modalController = ({ modal, btnOpen, btnClose, time = 300 }) => {
+	const buttonElems = document.querySelectorAll(btnOpen);
+	const modalElem = document.querySelector(modal);
+
+	modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity ${time}ms ease-in-out;
+  `;
+
+	const closeModal = event => {
+		const target = event.target;
+
+		if (
+			target === modalElem ||
+			(btnClose && target.closest(btnClose)) ||
+			event.code === 'Escape'
+		) {
+
+			modalElem.style.opacity = 0;
+
+			setTimeout(() => {
+				modalElem.style.visibility = 'hidden';
+				scrollController.enabledScroll();
+			}, time);
+
+			window.removeEventListener('keydown', closeModal);
+		}
+	}
+
+	const openModal = () => {
+		modalElem.style.visibility = 'visible';
+		modalElem.style.opacity = 1;
+		window.addEventListener('keydown', closeModal);
+		scrollController.disabledScroll();
+	};
+
+	buttonElems.forEach(btn => {
+		btn.addEventListener('click', openModal);
+	});
+
+	modalElem.addEventListener('click', closeModal);
+};
+
+modalController({
+	modal: ".modal", btnOpen: '.js-order', btnClose: ".modal__close", time: 300
+})
